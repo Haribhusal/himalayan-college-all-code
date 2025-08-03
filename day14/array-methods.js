3ee130eeb0d3bf5f5cca758e59c03aa7ed7bd9d9
Haribhusal <bhusalhari89@gmail.com>
2025-08-03 09:06:39 +0545

    new project

---
+++ day14/array-methods.js
@@ -0,0 +1,47 @@
+// let fruits = ["Apple", "Banana", "Orange", "Mango", "Kiwi"]
+// fruits.sort();
+// console.log(fruits)
+
+// let randomNumbers = [30, 15, 56, 90, 32, 52, 65, 13, 9, 67]
+// randomNumbers.sort(function (a, b) { return a - b });
+// console.log(randomNumbers)
+
+
+//Foreach Method
+// let fruits = ["Apple", "Banana", "Orange", "Mango", "Kiwi"]
+
+// let allFruits = ''
+// fruits.forEach((fruit) => {
+//     allFruits += fruit + ' '
+// })
+
+// console.log(allFruits)
+
+
+// Array Map
+// let numbers = [5, 8, 3, 9, 2, 4]
+// let newSquareNumbers = numbers.map((item) => item * item)
+// console.log(newSquareNumbers)
+
+
+
+// let fruits = ["Apple", "Banana", "Orange", "Mango", "Kiwi"];
+// // fruits.push('Grapes')
+// fruits.pop();
+
+// console.log(fruits)
+
+
+// let fruits = ["Apple", "Banana", "Orange", "Mango", "Kiwi"];
+// // fruits.unshift('Papaya')
+// fruits.shift();
+// console.log(fruits)
+
+
+let fruits = ["Apple", "Banana", "Orange", "Mango", "Kiwi"];
+// fruits.splice(2, 0, "Lemon")
+// console.log(fruits)
+let sliced = fruits.slice(0, 2)
+console.log(sliced)
+
+
---
+++ day14/array.js
@@ -0,0 +1,26 @@
+let user1 = {
+    name: "Ram",
+    age: 23
+}
+
+let user2 = {
+    name: "Shyam",
+    age: 24
+}
+let user3 = {
+    name: "Ramesh",
+    age: 25
+}
+
+let allUsers = [user1, user2, user3, [1, 2, 3], "Prabesh", true, 1]
+
+
+let fruits = ["Apple", "Banana", "Orange", "Mango", "Kiwi"]
+// Accessing items of array
+console.log(fruits[3])
+
+// count all items
+console.log(fruits.length)
+
+// print last item
+console.log(fruits[fruits.length - 1])
